ace2861c958e23f3894671499358cb6c0f6a0c86
bingryan <bingryan@outlook.com>
2021-08-19 22:40:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png"/>
+  <HelloWorld msg="Hello Vue 3 + Vite"/>
+  <button
+      class="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">
+    Tailwind Button
+  </button>
+  <button class="btn btn-primary rounded-full">DaisyUI Button</button>
+
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
